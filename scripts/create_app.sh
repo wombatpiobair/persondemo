4cc535419abba9c349afa3f69090ad6cad2c3b2e
bcombs <bob.combs@oracle.com>
2023-01-11 16:41:31 -0800

    remove java version and use default

--- scripts/create_app.sh
+++ scripts/create_app.sh
@@ -7,5 +7,4 @@ az group create --resource-group demo --location eastus
 az spring create --resource-group demo --name person
 
 #create the app
-az spring app create --resource-group demo --service person --name pdemo --assign-endpoint true \
-  --runtime-version Java_17
+az spring app create --resource-group demo --service person --name pdemo --assign-endpoint true
